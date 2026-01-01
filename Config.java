d93497dbe2e412d562184315db2840163bef4790
thricvz <thricvz@gmail.com>
2026-01-01 19:08:34 +0100

    Increased screenSize

--- Config.java
+++ Config.java
@@ -1,7 +1,7 @@
 
 
 public class Config{
-  public static int blockSize = 32;
+  public static int blockSize = 64;
   public static int screenHeight = blockSize*21;
   public static int screenWidth = blockSize * 15 ;
 
